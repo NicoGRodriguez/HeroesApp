97c1d08d7b745a72075be5db98cd51d3267965b9
nico <nicolas_rodriguez_94@hotmail.com>
2022-05-05 16:31:29 -0300

    se agrego nuevos modulos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { HeroeImagenPipe } from './pipes/heroe-imagen.pipe';
 
 
 
@@ -15,10 +22,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    HeroeImagenPipe
   ],
   imports: [
-    CommonModule, HeroesRoutingModule
+    CommonModule, FlexLayoutModule, FormsModule, HeroesRoutingModule, MaterialModule
   ]
 })
 export class HeroesModule { }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Heroe } from '../interfaces/heroes.interface';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private urlHeroe: string = environment.baseUrl;
+
+  get httpParams() {
+    return new HttpParams().set('', '');
+  }
+
+  constructor(private http: HttpClient) { }
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.urlHeroe}/heroes`)
+  }
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.urlHeroe}/heroes/${id}`);
+  }
+  getSugerencia(q: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.urlHeroe}/heroes?q=${q}&_limit=6}`);
+  }
+
+}
